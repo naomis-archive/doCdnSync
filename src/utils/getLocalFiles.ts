b81b82b99ea805b940b815b8b6fdfc2b99ed6e0c
zaida04 <nico.03727+github@gmail.com>
2022-03-17 23:36:39 -0400

    chore: weird prettier errors

--- src/utils/getLocalFiles.ts
+++ src/utils/getLocalFiles.ts
@@ -19,8 +19,8 @@ const walkDirectory = async (dir: string): Promise<string[]> => {
 /**
  * Module to generate the local file list for the contents directory.
  *
- * @param baseDir
- * @returns {Promise<string[]>} An array containing file paths for everything in `contents`.
+ * @param {string} baseDir Base directory to look for `content` in.
+ * @returns {Promise<string[]>} An array containing file paths for everything in `content`.
  */
 export const getLocalFiles = async (baseDir: string) => {
   const contents = await walkDirectory(join(baseDir, "content"));
